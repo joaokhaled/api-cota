644ac0fe145d5b8569c34f918ea84794cbf4ca1e
Khaled <31672356+joaokhaled@users.noreply.github.com>
2018-06-18 20:38:28 -0300

    Update api.py

--- api.py
+++ api.py
@@ -1,3 +1,4 @@
+import os
 from scrapy import *
 from flask import Flask, jsonify
 
@@ -31,4 +32,5 @@ def api():
 				)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
