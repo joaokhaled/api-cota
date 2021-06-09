333d62d58352c24052df606424523b824312aee5
João Victor <31672356+joaokhaled@users.noreply.github.com>
2021-06-09 14:05:26 -0300

    Update api.py

--- api.py
+++ api.py
@@ -8,28 +8,28 @@ app = Flask(__name__)
 @app.route('/')
 def api():
 	return jsonify(
-					[
-						{
-							'moedas':
-								{
-								'dolar': f'{k[1].text}', 
-					 			'dolar-au': f'{k2[1].text}', 
-					 			'dolar-cad': f'{k3[1].text}', 
-					 			'dolar-nzd': f'{k4[1].text}', 
-					 			'eur': f'{k5[1].text}', 
-					 			'iene': f'{k6[1].text}', 
-					 			'libra': f'{k7[1].text}', 
-					 			'ouro': f'{k8[1].text}', 
-					 			'peso-arg': f'{k9[1].text}', 
-					 			'peso-chi': f'{k0[1].text}', 
-					 			'peso-uru': f'{ka[1].text}', 
-					 			'won-sul coreano': f'{kb[1].text}', 
-					 			'yuan': f'{kc[1].text}'
-					 			},
-					 		'update': ’upd'
-						 }						 						 		
-					] 
-				)
+			[
+			   {
+		               'moedas':
+					{
+					     'dolar': f'{k[1].text}', 
+					     'dolar-au': f'{k2[1].text}', 
+					     'dolar-cad': f'{k3[1].text}', 
+					     'dolar-nzd': f'{k4[1].text}', 
+					     'eur': f'{k5[1].text}', 
+					     'iene': f'{k6[1].text}', 
+					     'libra': f'{k7[1].text}', 
+					     'ouro': f'{k8[1].text}', 
+					     'peso-arg': f'{k9[1].text}', 
+					     'peso-chi': f'{k0[1].text}', 
+					     'peso-uru': f'{ka[1].text}', 
+					     'won-sul coreano': f'{kb[1].text}', 
+					     'yuan': f'{kc[1].text}'
+					 },
+				'update': ’upd'
+			   }						 						 		
+			] 
+		    )
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
